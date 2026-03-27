a2219bf85e1ea18c8cfd6f3d2fff801b249f908d
Yael Huamani <yaelyusefhm123@gmail.com>
2026-03-27 03:51:20 -0500

    feat: variables añadidas en script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log('Hola desde JavaScript!');
+
+let nombre = 'Yael';
+
+let edad = 20;
+
+let estaAprendiendo = true;
+
+const curso = 'Code 101';
+
+const maxIntentos = 10;
+
+console.log(nombre);
+
+console.log(edad);
+
+console.log(estaAprendiendo);
+
+console.log(typeof nombre);
+
+console.log(typeof edad);
+
+console.log(typeof estaAprendiendo);
+
+console.log("Hola soy", nombre);
+
+console.log(curso);
+
+console.log(maxIntentos);
